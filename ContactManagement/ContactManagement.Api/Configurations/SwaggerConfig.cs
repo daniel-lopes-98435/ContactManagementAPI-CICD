905021f46009ced3ede7be417be8645352bef527
daniel-lopes-98435 <43936124+daniel-lopes-98435@users.noreply.github.com>
2025-01-11 20:28:23 -0300

    Teste

--- ContactManagement/ContactManagement.Api/Configurations/SwaggerConfig.cs
+++ ContactManagement/ContactManagement.Api/Configurations/SwaggerConfig.cs
@@ -7,7 +7,7 @@ public static class SwaggerConfig
     {
         services.AddSwaggerGen(c =>
         {
-            c.SwaggerDoc("v1", new OpenApiInfo { Title = "Desafio Fase 1 Grupo2", Version = "v1" });
+            c.SwaggerDoc("v1", new OpenApiInfo { Title = "Desafio Fase 2 Grupo2", Version = "v1" });
 
             var xmlFile = $"{System.Reflection.Assembly.GetExecutingAssembly().GetName().Name}.xml";
             var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
