e5673e8b3946ad48b6a7acc96aef01a7a73dcea3
daniel-lopes-98435 <43936124+daniel-lopes-98435@users.noreply.github.com>
2025-01-14 10:52:04 -0300

    Teste

--- ContactManagement/ContactManagement.Api/Configurations/IdentityConfig.cs
+++ ContactManagement/ContactManagement.Api/Configurations/IdentityConfig.cs
@@ -7,7 +7,21 @@ public static class IdentityConfig
 {
     public static IServiceCollection AddIndentity(this IServiceCollection services, IConfiguration configuration)
     {
-        var key = Encoding.ASCII.GetBytes(configuration.GetValue<string>("SecretJWT"));
+        //Para rodar localmente
+        //var key = Encoding.ASCII.GetBytes(configuration.GetValue<string>("SecretJWT"));
+        
+        #region Teste para App service
+        // Se estiver no ambiente de produção, obtém a variável de ambiente
+        var secretJWT = configuration.GetValue<string>("SecretJWT");
+
+        if (string.IsNullOrEmpty(secretJWT))
+        {
+            // Se o segredo estiver ausente, pode lançar um erro ou lidar de outra forma
+            throw new ArgumentNullException("SecretJWT", "O segredo JWT não pode ser nulo ou vazio.");
+        }
+        #endregion
+
+        var key = Encoding.ASCII.GetBytes(secretJWT);
 
         services.AddAuthentication(x =>
         {
